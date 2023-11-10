3cfa9871f52e309964c132327bd9fccfe4a6ad69
Ryan DeVore <ryan@silverpine.com>
2023-11-10 08:25:12 -0800

    Rename to match serializer naming convention

--- library/src/main/java/com/silverpine/uu/core/UULongDateSerializer.kt
+++ library/src/main/java/com/silverpine/uu/core/UULongDateSerializer.kt
@@ -8,12 +8,12 @@ import kotlinx.serialization.encoding.Encoder
 import java.util.Locale
 import java.util.TimeZone
 
-class UULongDateJsonAdapter(
+class UULongDateSerializer(
     private val format: String = UUDate.RFC_3999_DATE_TIME_WITH_MILLIS_FORMAT,
     private val timeZone: TimeZone = UUDate.UTC_TIME_ZONE,
     private val locale: Locale = Locale.US): KSerializer<Long>
 {
-    override val descriptor = PrimitiveSerialDescriptor("UULongDateJsonAdapter", PrimitiveKind.LONG)
+    override val descriptor = PrimitiveSerialDescriptor("UULongDateSerializer", PrimitiveKind.LONG)
     override fun serialize(encoder: Encoder, value: Long)
     {
         encoder.encodeString(value.uuFormatDate(format, timeZone, locale))
