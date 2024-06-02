fff4fa08cb66a4465292a1cd03cc27a7087334dd
Ryan DeVore <ryan@silverpine.com>
2024-06-02 08:04:55 -0700

    Add read all from stream method

--- library/src/main/java/com/silverpine/uu/core/UUStream.kt
+++ library/src/main/java/com/silverpine/uu/core/UUStream.kt
@@ -3,6 +3,7 @@ package com.silverpine.uu.core
 import android.text.TextUtils
 import com.silverpine.uu.logging.UULog
 import java.io.BufferedInputStream
+import java.io.ByteArrayOutputStream
 import java.io.FileOutputStream
 import java.io.InputStream
 import java.io.OutputStream
@@ -63,4 +64,38 @@ fun InputStream.uuUnzip(destinationFolder: Path)
     {
         UULog.d(javaClass, "uuUnzip", "", ex)
     }
+}
+
+fun InputStream.uuReadAll(): ByteArray?
+{
+    var result: ByteArray? = null
+
+    try
+    {
+        val outputStream = ByteArrayOutputStream()
+        outputStream.use()
+        { os ->
+
+            val buffer = ByteArray(10240)
+            var length = 1
+
+            while (length > 0)
+            {
+                length = read(buffer)
+
+                if (length > 0)
+                {
+                    os.write(buffer, 0, length)
+                }
+            }
+
+            result = os.toByteArray()
+        }
+    }
+    catch (ex: Exception)
+    {
+        UULog.d(javaClass, "uuReadAll", "", ex)
+    }
+
+    return result
 }
